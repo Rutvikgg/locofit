7dddad14345c4940239bd52d9631004d2761beab
Rutvik Gondekar <gondekarrutvik@gmail.com>
2023-09-26 14:12:16 +0530

    First Commit

---
+++ app/src/main/java/com/rutvik/locofit/MainActivity.java
@@ -0,0 +1,24 @@
+package com.rutvik.locofit;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Hello from Toast", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
